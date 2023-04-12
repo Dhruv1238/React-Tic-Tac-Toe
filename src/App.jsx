26c05de3c5c8d9405026812ad1a141893512d7bd
dhruv1238 <dhruv4075@gmail.com>
2023-04-12 22:45:54 +0530

    use state to store x in each box

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function board() {
 
   const[squares,setSquares]=useState(Array(9).fill(null));
 
-  function handleClick(){
+  function handleClick(i){
     const nexSquares= squares.slice();
     nexSquares[i]="X";
     setSquares(nexSquares)
@@ -27,19 +27,19 @@ function board() {
   return (
   <>
     <div>
-      <Square value={squares[0]} onSquareClick={handleClick}  />
-      <Square value={squares[1]} onSquareClick={handleClick}  />
-      <Square value={squares[2]} onSquareClick={handleClick}  />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}  />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}  />
     </div>
     <div>
-      <Square value={squares[3]} onSquareClick={handleClick}  />
-      <Square value={squares[4]} onSquareClick={handleClick} />
-      <Square value={squares[5]} onSquareClick={handleClick}  />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}  />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}  />
     </div>
     <div>
-      <Square value={squares[6]} onSquareClick={handleClick}  />
-      <Square value={squares[7]} onSquareClick={handleClick}  />
-      <Square value={squares[8]} onSquareClick={handleClick}  />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}  />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}  />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}  />
     </div>
   </>
   );
